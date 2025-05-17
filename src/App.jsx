0c38b5cdb5c4d301db03554a8934add291216fc4
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-05-18 03:46:24 +0530

    creating the marker with movement

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import React from 'react';
 import Map from './components/Map';
 import CurrentMarker from './components/CurrentMarker';
 import MultipleMarkers from './components/MultipleMarkers';
+import MovingMarker from './components/MovingMarker';
 
 function App() {
 
-  const locations = [
-    { lat: 28.6139, lng: 77.2090 }, // Delhi
-    { lat: 19.0760, lng: 72.8777 }, // Mumbai
-    { lat: 12.9716, lng: 77.5946 }, // Bangalore
-    { lat: 13.0827, lng: 80.2707 }, // Chennai
-  ];
+  // const locations = [
+  //   { lat: 28.6139, lng: 77.2090 }, // Delhi
+  //   { lat: 19.0760, lng: 72.8777 }, // Mumbai
+  //   { lat: 12.9716, lng: 77.5946 }, // Bangalore
+  //   { lat: 13.0827, lng: 80.2707 }, // Chennai
+  // ];
 
 
   return (
@@ -20,7 +21,8 @@ function App() {
       <h1>Google Map with Marker</h1>
       {/* <Map lat={28.6139} lng={77.2090} /> */}
       {/* <CurrentMarker/> */}
-      <MultipleMarkers locations={locations} />
+      {/* <MultipleMarkers locations={locations} /> */}
+      {/* <MovingMarker/> */}
     </div>
   );
 }
