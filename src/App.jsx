d50ebf23e52e05c6416fe876b193ff3d253f9a38
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-05-18 03:38:56 +0530

    showing the current location using google map

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 
 import React from 'react';
 import Map from './components/Map';
+import CurrentMarker from './components/CurrentMarker';
 
 function App() {
   return (
     <div>
       <h1>Google Map with Marker</h1>
-      <Map lat={28.6139} lng={77.2090} /> {/* Example: Delhi */}
+      {/* <Map lat={28.6139} lng={77.2090} /> */}
+      <CurrentMarker/>
     </div>
   );
 }
