22842d05e4e31f0ac59a07fbd8e09e9e13686e8a
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-05-18 03:40:42 +0530

    showing the multiple markers on google map

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,24 @@
 import React from 'react';
 import Map from './components/Map';
 import CurrentMarker from './components/CurrentMarker';
+import MultipleMarkers from './components/MultipleMarkers';
 
 function App() {
+
+  const locations = [
+    { lat: 28.6139, lng: 77.2090 }, // Delhi
+    { lat: 19.0760, lng: 72.8777 }, // Mumbai
+    { lat: 12.9716, lng: 77.5946 }, // Bangalore
+    { lat: 13.0827, lng: 80.2707 }, // Chennai
+  ];
+
+
   return (
     <div>
       <h1>Google Map with Marker</h1>
       {/* <Map lat={28.6139} lng={77.2090} /> */}
-      <CurrentMarker/>
+      {/* <CurrentMarker/> */}
+      <MultipleMarkers locations={locations} />
     </div>
   );
 }
